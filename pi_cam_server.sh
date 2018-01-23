afdfdaf89168536c4986153d1c0103d4f77fb6d1
Callum Gundlach <gundlachcallum@gmail.com>
2018-01-23 17:25:06 -0500

    Added pi cam server script and camera config

---
+++ pi_cam_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export ROS_IP=$(hostname -I) #Set to ip of this machine
+
+MASTER_IP=$(hostname -I) #Current ip of the pi
+
+export ROS_MASTER_URI=http://${MASTER_IP}:11311
+
+tmux new -d -s roscore 'roscore' #Start roscore in tmux terminal
+#roscore &
+sleep 10
+roslaunch raspicam_node camerav1_1280x720.launch #Start camera node
